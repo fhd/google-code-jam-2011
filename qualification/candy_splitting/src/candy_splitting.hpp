2b331de2ea82b2fd6ee90501df5fe237469df19c
Felix H. Dahlke <fhd@ubercode.de>
2011-05-08 00:56:08 +0200

    Passed all sample test cases.

--- qualification/candy_splitting/src/candy_splitting.hpp
+++ qualification/candy_splitting/src/candy_splitting.hpp
@@ -1,4 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+inline int patrick_sum(std::vector<int>& numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++)
+        sum ^= numbers[i];
+    return sum;
+}
+
+inline int sum(std::vector<int>& numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++)
+        sum += numbers[i];
+    return sum;
+}
+
 inline int candy_to_keep(std::vector<int>& candy)
 {
+    for (int i = 0; i < candy.size(); i++) {
+        std::vector<int> sean(candy), patrick;
+        for (int j = 0; j <= i; j++) {
+            patrick.push_back(sean[j]);
+            sean[j] = 0;
+        }
+
+        if (!patrick.empty() && !sean.empty()
+            && patrick_sum(patrick) == patrick_sum(sean))
+            return sum(sean);
+    }
     return 0;
 }
--- qualification/candy_splitting/test/candy_splitting_test.cpp
+++ qualification/candy_splitting/test/candy_splitting_test.cpp
@@ -7,9 +7,38 @@
 
 using namespace boost::assign;
 
+BOOST_AUTO_TEST_CASE(test_patrick_sum_1)
+{
+    std::vector<int> numbers;
+    numbers += 4, 5;
+    BOOST_REQUIRE_EQUAL(1, patrick_sum(numbers));
+}
+
+BOOST_AUTO_TEST_CASE(test_patrick_sum_2)
+{
+    std::vector<int> numbers;
+    numbers += 7, 9;
+    BOOST_REQUIRE_EQUAL(14, patrick_sum(numbers));
+}
+
+BOOST_AUTO_TEST_CASE(test_patrick_sum_3)
+{
+    std::vector<int> numbers;
+    numbers += 50, 10;
+    BOOST_REQUIRE_EQUAL(56, patrick_sum(numbers));
+}
+
 BOOST_AUTO_TEST_CASE(test_sample_1)
 {
     std::vector<int> candy;
     candy += 1, 2, 3, 4, 5;
     BOOST_REQUIRE_EQUAL(0, candy_to_keep(candy));
 }
+
+BOOST_AUTO_TEST_CASE(test_sample_2)
+{
+    std::vector<int> candy;
+    candy += 3, 5, 6;
+    BOOST_REQUIRE_EQUAL(11, candy_to_keep(candy));
+}
+
