af6bc348e46dc62a1e6c76a5809248587da42add
Felix H. Dahlke <fhd@ubercode.de>
2011-05-07 21:04:26 +0200

    Implemented I/O.

--- qualification/magicka/src/main.cpp
+++ qualification/magicka/src/main.cpp
@@ -12,6 +12,29 @@
 
 namespace fs = boost::filesystem;
 
+Combination parse_combination(const std::string& input)
+{
+    return Combination(input[0], input[1], input[2]);
+}
+
+Opposition parse_opposition(const std::string& input)
+{
+    return Opposition(input[0], input[1]);
+}
+
+template<class T>
+std::string collection_to_string(T collection)
+{
+    std::stringstream ss;
+    for (typename T::iterator it = collection.begin(); it != collection.end();
+         it++) {
+        if (it != collection.begin())
+            ss << ", ";
+        ss << *it;
+    }
+    return ss.str();
+}
+
 void process(const std::string& input_file, const std::string& output_file)
 {
    std::ifstream input(input_file.c_str());
@@ -22,7 +45,35 @@ void process(const std::string& input_file, const std::string& output_file)
       if (line.empty())
          continue;
 
-      // TODO: I/O
+      typedef boost::split_iterator<std::string::iterator>
+          string_split_iterator;
+      string_split_iterator it = boost::make_split_iterator(
+          line, boost::first_finder(" ", boost::is_equal()));
+
+      int num_combinations = boost::lexical_cast<int>(*it++);
+      std::vector<Combination> combinations;
+      for (int j = 0; j < num_combinations; j++) {
+          std::string s = boost::lexical_cast<std::string>(*it++);
+          combinations.push_back(parse_combination(s));
+      }
+
+      int num_oppositions = boost::lexical_cast<int>(*it++);
+      std::vector<Opposition> oppositions;
+      for (int j = 0; j < num_oppositions; j++) {
+          std::string s = boost::lexical_cast<std::string>(*it++);
+          oppositions.push_back(parse_opposition(s));
+      }
+
+      int num_elements = boost::lexical_cast<int>(*it++);
+      std::string element_str = boost::lexical_cast<std::string>(*it++);
+      std::vector<char> elements;
+      for (int j = 0; j < num_elements; j++)
+          elements.push_back(element_str[j]);
+
+      output << "Case #" << i << ": ["
+             << collection_to_string(invoke(elements, combinations,
+                                            oppositions))
+             << "]" << std::endl;
    }
 }
 
