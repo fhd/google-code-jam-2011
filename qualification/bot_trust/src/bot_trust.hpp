3a1ca07debf5f9396fc4ef32b4526113609859c0
Felix H. Dahlke <fhd@ubercode.de>
2011-05-07 10:02:08 +0200

    Implemented bot trust problem.

--- qualification/bot_trust/src/bot_trust.hpp
+++ qualification/bot_trust/src/bot_trust.hpp
@@ -1,15 +1,63 @@
 #include <vector>
 #include <utility>
+#include <queue>
+#include <boost/foreach.hpp>
+#include <boost/shared_ptr.hpp>
 
-enum Robot {
+enum Robots {
     ORANGE,
     BLUE
 };
 
-inline int time_required_for(std::vector<std::pair<Robot, int> >& sequence)
+struct Robot {
+    int pos;
+    std::queue<int> buttons;
+
+    Robot() : pos(1) {}
+};
+
+inline int move_toward(int start, int goal) {
+    if (start == goal)
+        return start;
+    return (start < goal) ? start + 1 : start - 1;
+}
+
+inline void move_robots(std::vector<std::pair<Robots, int> >::iterator& it,
+                        Robot& orange, Robot& blue)
+{
+    bool orange_active = it->first == ORANGE;
+    Robot& active_robot = orange_active ? orange : blue;
+    Robot& inactive_robot = orange_active ? blue : orange;
+
+    int current_button = it->second;
+    if (active_robot.pos == current_button) {
+        // Button pressed
+        if (active_robot.buttons.size() > 0)
+            active_robot.buttons.pop();
+        it++;
+    } else
+        active_robot.pos = move_toward(active_robot.pos, current_button);
+
+    inactive_robot.pos = move_toward(inactive_robot.pos,
+                                     inactive_robot.buttons.front());
+}
+
+inline int time_required_for(std::vector<std::pair<Robots, int> >& sequence)
 {
     if (sequence.size() == 0)
         return 0;
 
-    return 6;
+    Robot orange, blue;
+    typedef std::pair<Robots, int> robot_pair;
+    BOOST_FOREACH (robot_pair p, sequence)
+        if (p.first == ORANGE)
+            orange.buttons.push(p.second);
+        else
+            blue.buttons.push(p.second);
+
+    int time = 0;
+    for (std::vector<robot_pair>::iterator it = sequence.begin();
+         it != sequence.end(); time++)
+        move_robots(it, orange, blue);
+    return time;
 }
--- qualification/bot_trust/test/bot_trust_test.cpp
+++ qualification/bot_trust/test/bot_trust_test.cpp
@@ -4,7 +4,7 @@
 #include <boost/test/unit_test.hpp>
 #include <bot_trust.hpp>
 
-typedef std::pair<Robot, int> robot_pair;
+typedef std::pair<Robots, int> robot_pair;
 typedef std::vector<robot_pair > robot_sequence;
 
 BOOST_AUTO_TEST_CASE(test_empty)
@@ -23,3 +23,19 @@ BOOST_AUTO_TEST_CASE(test_sample_1)
     BOOST_REQUIRE_EQUAL(6, time_required_for(sequence));
 }
 
+BOOST_AUTO_TEST_CASE(test_sample_2)
+{
+    robot_sequence sequence;
+    sequence.push_back(robot_pair(ORANGE, 5));
+    sequence.push_back(robot_pair(ORANGE, 8));
+    sequence.push_back(robot_pair(BLUE, 100));
+    BOOST_REQUIRE_EQUAL(100, time_required_for(sequence));
+}
+
+BOOST_AUTO_TEST_CASE(test_sample_3)
+{
+    robot_sequence sequence;
+    sequence.push_back(robot_pair(BLUE, 2));
+    sequence.push_back(robot_pair(BLUE, 1));
+    BOOST_REQUIRE_EQUAL(4, time_required_for(sequence));
+}
