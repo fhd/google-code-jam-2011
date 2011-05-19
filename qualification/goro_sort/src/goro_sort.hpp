9112dfe96cf9d9395272db9bff49184996c355fa
Felix H. Dahlke <fhd@ubercode.de>
2011-05-19 07:00:36 +0200

    Added the goro sort problem.

---
+++ qualification/goro_sort/src/goro_sort.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+inline int number_of_hits(std::vector<int>& elements)
+{
+    return elements.size();
+}
---
+++ qualification/goro_sort/test/goro_sort_test.cpp
@@ -0,0 +1,29 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+#include <boost/assign.hpp>
+#include <goro_sort.hpp>
+
+using namespace boost::assign;
+
+BOOST_AUTO_TEST_CASE(test_sample_1)
+{
+    std::vector<int> elements;
+    elements += 2, 1;
+    BOOST_REQUIRE_EQUAL(2, number_of_hits(elements));
+}
+
+BOOST_AUTO_TEST_CASE(test_sample_2)
+{
+    std::vector<int> elements;
+    elements += 1, 3, 2;
+    BOOST_REQUIRE_EQUAL(3, number_of_hits(elements));
+}
+
+BOOST_AUTO_TEST_CASE(test_sample_3)
+{
+    std::vector<int> elements;
+    elements += 2, 1, 4, 3;
+    BOOST_REQUIRE_EQUAL(4, number_of_hits(elements));
+}
